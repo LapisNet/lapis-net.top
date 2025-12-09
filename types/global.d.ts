bc4a0d3779cca76e1bf1c7204eb1c560b1e3fb46
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-12-10 05:23:18 +0800

    chore: add types

---
+++ types/global.d.ts
@@ -0,0 +1,50 @@
+type Project = {
+	/**
+	 * 状态
+	 * @default 'unknown'
+	 */
+	_status?: 'unknown' | 'wip' | 'normal' | 'archived' | 'deprecated';
+	/**
+	 * 可见性
+	 * @default true
+	 */
+	_show?: boolean;
+	/** 项目名称/标题 */
+	title: string;
+	/** 副标题 */
+	sub_title?: string;
+	/** 信息行1 */
+	info1?: string;
+	/** 信息行2 */
+	info2?: string;
+	/**
+	 * 仓库短链接
+	 * @example 'username/repo'
+	 */
+	repo?: string;
+	/**
+	 * 仓库类型
+	 * @default 'github.com'
+	 */
+	repo_type?: string;
+	/** 许可证 */
+	license?: string;
+};
+
+type Member = {
+	/**
+	 * 可见性
+	 * @default true
+	 */
+	_show?: boolean;
+	/** 头像外链 */
+	avatar: string | null;
+	/** 昵称 */
+	name: string;
+	/** 别名 */
+	aka?: string;
+	/** 身份 */
+	role: string;
+	/** 个人主页 */
+	url?: string;
+};
