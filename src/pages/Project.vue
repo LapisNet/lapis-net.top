0713559bc3211f1cabea8598aa3a7e43467b7b29
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-12-10 07:58:41 +0800

    feat(project): add status badge and sync new rules

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,6 +1,8 @@
 <script setup>
 import {ref, onBeforeMount} from 'vue';
+import { statusTexts } from '../shared';
 
+/** @type {Project[]} */
 const projects = ref([]);
 onBeforeMount(async() => {
 	const res = await fetch('https://ghfast.top/https://raw.githubusercontent.com/LapisNet/.public_data/refs/heads/main/projects.json');
@@ -14,20 +16,21 @@ onBeforeMount(async() => {
 		<div class="content">
 			<p class="loading" v-if="projects.length === 0">正在加载...</p>
 			<div id="pj-ls" :style="`grid-template-rows: repeat(${Math.ceil(projects.length / 2)}, 1fr);`" v-else>
-				<div class="card card-project" v-for="item in projects" :key="item.title" v-show="item._show">
+				<div class="card card-project" v-for="item in projects" :key="item.title" v-show="typeof item._show === 'undefined' || item._show">
 					<img class="pj-icon" :src="item.icon" :alt="item.title" v-if="!!item.icon" />
 					<span class="pj-name">
 						<div class="pj-title">{{item.title}}</div>
-						<div class="pj-subtitle" :innerHTML="item.subTitle"></div>
+						<div class="pj-subtitle" :innerHTML="item.sub_title"></div>
+						<span class="status-badge" title="状态" :data-type="item._status ?? 'unknown'">{{ statusTexts[item._status ?? 'unknown'] }}</span>
 					</span>
 					<div class="pj-desc">
 						<div class="info1" :innerHTML="item.info1"></div>
 						<div class="info2" :innerHTML="item.info2"></div>
 					</div>
 					<div class="pj-repo" v-if="!!item.repo">
-						<a :class="`fab fa-${item.repotype || 'github'}`" :href="`https://${item.repotype || 'github'}.com/${item.repo}`" target="_blank" rel="noopener noreferrer">访问仓库</a>
+						<a :class="`fab fa-${item.repotype || 'github'}`" :href="`https://${item.repotype || 'github.com'}/${item.repo}`" target="_blank" rel="noopener noreferrer">访问仓库</a>
 					</div>
-				</div>
+				</div><br />
 				<p class="no-more">没有更多项目了...</p>
 			</div>
 		</div>
@@ -35,9 +38,6 @@ onBeforeMount(async() => {
 </template>
 
 <style lang="scss" scoped>
-#main {
-	margin-bottom: 0.4em;
-}
 #pj-ls {
 	display: grid;
 	align-items: center;
---
+++ src/shared/index.js
@@ -0,0 +1,7 @@
+export const statusTexts = {
+	unknown: '❔待确定',
+	wip: '🚧开发中',
+	normal: '✅活跃中',
+	deprecated: '⚠️已放弃',
+	archived: '💾已停更'
+};
