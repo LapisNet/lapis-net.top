9c57ed5b3749f9e7c4cee5ee6afc9339bfcfd5d3
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-12-09 19:58:36 +0800

    chore(footer): add banner

--- src/app/Footer.vue
+++ src/app/Footer.vue
@@ -39,7 +39,10 @@ document.addEventListener('touchmove', (e) => { // 移动端触摸移动
 
 <template>
 	<div id="footer">
-		<p>Copyright (c) 2025 LapisNet. All rights reserved.</p>
+		<div class="content">
+			<img class="banner" title="LapisNet" src="/res/banner.webp" />
+			<p>Copyright (c) 2025 LapisNet. All rights reserved.</p>
+		</div>
 		<div class="content" data-title="我们用到的技术"><br>
 			<a href="https://vuejs.org" target="_blank" rel="noopener noreferrer">Vue.js</a><br>
 			<a href="https://picocss.com" target="_blank" rel="noopener noreferrer">Pico CSS</a><br>
@@ -64,7 +67,7 @@ document.addEventListener('touchmove', (e) => { // 移动端触摸移动
 #footer {
 	padding: 1em 0.4em;
 	display: grid;
-	align-items: end;
+	align-items: start;
 	justify-content: space-between;
 	grid-template-columns: 22.6em 15% 15% 15%;
 	font-size: 16px;
@@ -84,9 +87,15 @@ document.addEventListener('touchmove', (e) => { // 移动端触摸移动
 }
 p {
 	margin: 0;
+	font-size: 0.8em;
+	text-align: center;
+	margin-top: 0.2em;
+}
+.banner {
+	filter: drop-shadow(2px 2px 2px rgba(0,0,0,0.2));
 }
 @media screen and (max-width: 768px) {
-	.content {
+	.content:not(:first-child), .banner {
 		display: none;
 	}
 }
