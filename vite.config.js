fda4f5d970861764eaf8dc47a9c079c83825a602
晚梦 <127290936+LateDreamXD@users.noreply.github.com>
2025-12-11 19:58:44 +0800

    perf: split chunks

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // vite.config.js
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
- 
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
@@ -9,6 +9,19 @@ export default defineConfig({
 		alias: {
 			'@': '/src',
 			'@pages': '/src/pages'
+		}
+	},
+	build: {
+		modulePreload: true,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					chunks: ['src/router/index.js'],
+					vue: ['vue'],
+					'vue-router': ['vue-router'],
+					json5: ['json5']
+				}
+			}
 		}
 	}
 });
